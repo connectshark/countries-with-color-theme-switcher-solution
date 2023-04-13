dcdc17f40a9e4067717de4d5f8d44191602e29a0
Chambers <bobhus394@gmail.com>
2023-04-13 14:57:49 +0800

    新增top level domain資料

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -16,11 +16,12 @@
       <div class="p-6 grid grid-cols-2 gap-4">
         <h2 class=" col-span-2 font-extrabold text-2xl mb-6">{{ data.name.common }}</h2>
         <p><span class="font-semibold">Population:</span> {{ data.population }}</p>
+        <p><span class="font-semibold">Top Level Domain:</span> {{ data.tld[0] }}</p>
         <p><span class="font-semibold">Region:</span> {{ data.region }}</p>
         <p><span class="font-semibold">Sub Region:</span> {{ data.subregion }}</p>
         <p><span class="font-semibold">Capital:</span> {{ data.capital[0] }}</p>
-        <p><span class="font-semibold">currencies:</span> {{ Object.keys(data.currencies)[0] }}</p>
-        <p><span class="font-semibold">languages:</span><span class="inline-block" v-for="language in Object.values(data.languages)">{{ language }}</span></p>
+        <p><span class="font-semibold">Currencies:</span> {{ Object.keys(data.currencies)[0] }}</p>
+        <p class=" space-x-2"><span class="font-semibold">Languages: </span><span class="inline-block" v-for="language in Object.values(data.languages)">{{ language }}</span></p>
       </div>
       <div class="p-6" v-if="borders.length > 0">
         <h3 class="font-extrabold">Border Countries:</h3>
